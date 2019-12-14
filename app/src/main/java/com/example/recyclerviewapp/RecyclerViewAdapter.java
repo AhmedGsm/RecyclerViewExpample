b143e81bfa5890df68090044e8a508f04ab680e2
AhmedGsm <Ahmed.gsm1983@gmail.com>
2019-12-14 21:21:03 +0100

    Implement onBindViewHolder method and replace the contents of textViews and imageView

--- app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
@@ -4,7 +4,9 @@ import android.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,6 +24,23 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         LinearLayout linearLayout = (LinearLayout )LayoutInflater.from(parent.getContext()).
                 inflate(R.layout.list_item,parent,false);
         return new CustomViewHolder(linearLayout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        //Display results in textView 1
+        TextView textView1 = holder.itemView.findViewById(R.id.textView1);
+        textView1.setText(mDataset[position]);
+
+        //Display results in textView 2
+        TextView textView2 = holder.itemView.findViewById(R.id.textView2);
+        textView2.setText(Integer.toString(position + 1));
+
+        //Display dummy image
+        LinearLayout linearLayoutParent = (LinearLayout)holder.itemView;
+        ImageView dummyImageView = (ImageView)linearLayoutParent.getChildAt(0);
+        dummyImageView.setImageResource(R.drawable.baseline_card_giftcard_black_48dp);
+
     }
 
     public static class CustomViewHolder extends RecyclerView.ViewHolder {
