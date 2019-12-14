d51d7bd0f5974d6e76dd76f566090d597ac022e8
AhmedGsm <Ahmed.gsm1983@gmail.com>
2019-12-14 20:32:29 +0100

    Set constructor to the recylcer view adapter

--- app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
@@ -7,6 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private String[] mDataset;
 
+    public RecyclerViewAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
     public static class CustomViewHolder extends RecyclerView.ViewHolder {
         public LinearLayout mLinearLayout;
         public CustomViewHolder(LinearLayout linearLayout) {
