f04e6c46bfec1882e6b7aed04d406c69eb2fca6b
AhmedGsm <Ahmed.gsm1983@gmail.com>
2019-12-14 20:10:51 +0100

    05-Create recycler view adapter class

---
+++ app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
@@ -0,0 +1,6 @@
+package com.example.recyclerviewapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.Adapter.ViewHolder> {
+}
