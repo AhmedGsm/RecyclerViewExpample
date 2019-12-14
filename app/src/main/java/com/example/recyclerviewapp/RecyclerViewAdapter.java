854a1743dbeb2c33cdd257aad95b1a02d2a2bfa6
AhmedGsm <Ahmed.gsm1983@gmail.com>
2019-12-14 20:26:17 +0100

    Set constructor to the custom ViewHolder

--- app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.java
@@ -1,9 +1,18 @@
 package com.example.recyclerviewapp;
 
+import android.widget.LinearLayout;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private String[] mDataset;
+
+    public static class CustomViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout mLinearLayout;
+        public CustomViewHolder(LinearLayout linearLayout) {
+            super(linearLayout);
+            mLinearLayout = linearLayout;
+        }
 
     public RecyclerViewAdapter(String[] myDataset) {
         mDataset = myDataset;
