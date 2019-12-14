76ed3efc16f4842e9f6583abde0db06d40b5897d
AhmedGsm <Ahmed.gsm1983@gmail.com>
2019-12-14 20:00:28 +0100

    03-Obtain handle to recycler view object

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.recyclerviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain handle to recycler view object
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
     }
 }
